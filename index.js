860856e1a54ffb39b84089df7a91a05867956f9f
Doug Mason <doug@norfielddp.com>
2022-12-09 14:30:48 -0500

    split files

---
+++ index.js
@@ -0,0 +1,13 @@
+const oups = require('./oups.json');
+const julie = require('./julie.json');
+const utah = require('./utah.json');
+const db = require('db.json')
+
+
+module.exports = () => ({
+    oups,
+    julie,
+    utah,
+    db
+    // Something more
+});
